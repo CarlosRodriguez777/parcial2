2dd3ab5baa518ff11925df2b005f7dfa13851538
Laura Alexandra Herrera Espitia <est.lauraa.herrerae@unimilitar.edu.co>
2026-04-24 09:36:30 -0500

    Estructura base HTML y configuración del Canvas.

---
+++ script.js
@@ -0,0 +1,10 @@
+/**
+ * Universidad - Facultad de Ingeniería
+ * Asignatura: Introducción a la Computación Gráfica
+ * Estudiante: Carlos Andres Rodriguez Diaz 
+ * Tarea: Implementar los algoritmos de rasterización manual.
+ */
+
+// Configuración inicial del Contexto 2D
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
