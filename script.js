16d023d73de6b4ff0f41a8228d921fd566e226d3
Laura Alexandra Herrera Espitia <est.lauraa.herrerae@unimilitar.edu.co>
2026-04-24 09:43:11 -0500

    Prueba de pixel y canva

--- script.js
+++ script.js
@@ -8,3 +8,13 @@
 // Configuración inicial del Contexto 2D
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "red";
+ctx.fillRect(10, 10, 50, 50);
+
+function drawPixel(ctx, x, y, color = "#000000") {
+    ctx.fillStyle = color;
+    ctx.fillRect(Math.floor(x), Math.floor(y), 1, 1);
+}
+
+drawPixel(ctx, 100, 100, "blue");
